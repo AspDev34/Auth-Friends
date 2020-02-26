256bc664656eeb296b0850fedcd191b6e2d886fd
Ike Otu <ikeotu@msn.com>
2020-02-26 12:36:46 -0800

    FriendsComp completed

---
+++ friends/src/components/FriendsComp.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { axiosWithAuth } from './utilities/axiosWithAuth';
+
+class FriendsComp extends React.Component {
+    state = {
+        friends: []
+    }
+
+    componentDidMount() {
+        this.getData();
+    };
+
+    getData = () => {
+        axiosWithAuth()
+            .get('/api/friends')
+            .then(res => {
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err => console.log('No data returned', err));
+    };
+
+    render() {
+        console.log('friends data from api', this.state.friends);
+        return (
+            <div>
+                {this.state.friends.map(item => (
+                    <p>{item.name}</p>
+                ))}
+            </div>
+        )
+    }
+};
+
+export default FriendsComp;
